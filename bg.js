d725dbab89faba1a4ba3e446f79a152cdf8fa90d
lees-lee <59959412+lees-lee@users.noreply.github.com>
2021-04-19 21:47:10 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,7 +8,7 @@ function handleImageLoad() {
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `Images/${imgNumber + 1}.jpg`;
+  image.src = `${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
 
   body.prepend(image);
